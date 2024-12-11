7c22903ef27f704e81ff4e4ebd6fe69c7bca981f
abhidev <abhidevramesh@gmail.com>
2024-12-11 23:18:51 +0530

    movement and 3d assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Diagnostics.Contracts;
-using System.Transactions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,36 +6,37 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float forwardSpeed = 0f;
     [SerializeField] Transform cam;
     bool isMoving = true;
+    bool moveCam = false;
     float speed = 1.5f;
     Vector3 Position;
+    Vector3 CamPosition;
+    [SerializeField] float distance;
     float x;
     Rigidbody frog;
     void Awake()
     {
         frog = GetComponent<Rigidbody>();
+        CamPosition = cam.position;
     }
     void Update()
     {
-        if(!isMoving)
+        if(moveCam)
         {
-        Debug.Log(Position);
-        cam.position = Position;
+        Debug.Log("Camera movement initiated");
+        float camPosition = Position.z - 3f;
+        cam.position = new Vector3(0,5.5f,camPosition);
+        moveCam = false;
         }
     }
-    public void ForwardJump(InputAction.CallbackContext context)
+    public void ForwardJump(InputAction.CallbackContext phase)
     {
-        Debug.Log(context.started);
         if(isMoving)
         {
-            Debug.Log("move is true");
-            if(!context.canceled)
+            Position = transform.position;
+            if(phase.started)
             {
-            frog.AddForce( Vector3.up * speed, ForceMode.Impulse);
-            frog.AddForce(Vector3.forward * forwardSpeed);
-            }
-            else if(context.canceled)
-            {
-            Position = transform.position ;
+                FrogJump();
+                Debug.Log(transform.position);
             }
         }
         isMoving = false;
@@ -47,5 +45,20 @@ public class PlayerMovement : MonoBehaviour
     private void Delay()
     {
         isMoving = true;
+        float f = Position.z;
+        float c = CamPosition.z;
+        x = f - c;
+        Debug.Log(x);
+        if(x >= distance)
+        {
+          moveCam = true;
+          Debug.Log("Frog out of sight");
+        }
+
+    }
+    public void FrogJump()
+    {
+            frog.AddForce( Vector3.up * speed, ForceMode.Impulse);
+            frog.AddForce(Vector3.forward * forwardSpeed);
     }
 }
